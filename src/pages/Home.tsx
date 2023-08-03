6b1618d5867f6c28e41d0af1ee654e6cb12d777c
nearworld <artemismars2@outlook.com>
2023-08-03 15:05:46 +0900

    style: install emotion and apply css for Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,16 @@
 import { Outlet } from "react-router-dom";
+import styled from "@emotion/styled";
 
 export const Home = () => {
+  const StyledMain = styled.main`
+    display: grid;
+    place-items: center;
+    height: 100vh;
+  `;
+
   return (
-    <div>
+    <StyledMain>
       <Outlet />
-    </div>
+    </StyledMain>
   );
 };
