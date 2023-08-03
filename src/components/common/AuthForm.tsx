e5e36841433b51bfd90f07824ad632306e9baae5
nearworld <artemismars2@outlook.com>
2023-08-03 15:40:32 +0900

    refactor: css 코드를 함수 컴포넌트 하단에 배치

--- src/components/common/AuthForm.tsx
+++ src/components/common/AuthForm.tsx
@@ -5,34 +5,6 @@ const FORM_WIDTH = 400;
 const FORM_PADDING = 30;
 
 export const AuthForm: FC<PropsWithChildren> = ({ children }) => {
-  const StyledForm = styled.form`
-    position: relative;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-    width: ${FORM_WIDTH}px;
-    height: 400px;
-    padding: ${FORM_PADDING}px;
-    border: 1px lightgray solid;
-  `;
-  const StyledSubmitButton = styled.button`
-    width: ${FORM_WIDTH}px;
-    height: 50px;
-
-    position: absolute;
-    bottom: ${FORM_PADDING}px;
-
-    color: white;
-    border: none;
-    cursor: pointer;
-    background-color: dodgerblue;
-
-    &:hover {
-      background-color: #0074e8;
-    }
-  `;
-
   return (
     <StyledForm>
       {children}
@@ -40,3 +12,31 @@ export const AuthForm: FC<PropsWithChildren> = ({ children }) => {
     </StyledForm>
   );
 };
+
+const StyledForm = styled.form`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  width: ${FORM_WIDTH}px;
+  height: 400px;
+  padding: ${FORM_PADDING}px;
+  border: 1px lightgray solid;
+`;
+const StyledSubmitButton = styled.button`
+  width: ${FORM_WIDTH}px;
+  height: 50px;
+
+  position: absolute;
+  bottom: ${FORM_PADDING}px;
+
+  color: white;
+  border: none;
+  cursor: pointer;
+  background-color: dodgerblue;
+
+  &:hover {
+    background-color: #0074e8;
+  }
+`;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,15 @@ import { Outlet } from "react-router-dom";
 import styled from "@emotion/styled";
 
 export const Home = () => {
-  const StyledMain = styled.main`
-    display: grid;
-    place-items: center;
-    height: 100vh;
-  `;
-
   return (
     <StyledMain>
       <Outlet />
     </StyledMain>
   );
 };
+
+const StyledMain = styled.main`
+  display: grid;
+  place-items: center;
+  height: 100vh;
+`;
