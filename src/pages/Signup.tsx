f96450d402b72c91d7746ace7ce9a1dadc0a516b
nearworld <artemismars2@outlook.com>
2023-08-04 13:01:16 +0900

    feat: 회원가입 페이지 구현

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,3 +1,31 @@
+import { AuthForm } from "../components/common/AuthForm";
+import { AuthInput } from "../components/common/AuthInput";
+import { useFormValidation } from "../hooks/useInputValidation";
+
 export const Signup = () => {
-  return <div>signup</div>;
+  const { handleSubmit, isBtnDisabled, validationResult, setValidationResult } =
+    useFormValidation();
+
+  return (
+    <section>
+      <AuthForm
+        title="회원가입"
+        testId="signup"
+        handleSubmit={handleSubmit}
+        isBtnDisabled={isBtnDisabled}
+        validationResult={validationResult}
+      >
+        <AuthInput
+          type="email"
+          testId="email"
+          setValidationResult={setValidationResult}
+        />
+        <AuthInput
+          type="password"
+          testId="password"
+          setValidationResult={setValidationResult}
+        />
+      </AuthForm>
+    </section>
+  );
 };
