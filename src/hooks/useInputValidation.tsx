d36da8379d7d2dd640f4a68496f09389768eddea
nearworld <artemismars2@outlook.com>
2023-08-04 12:57:18 +0900

    useInputValidation 함수에 handleSubmit 함수 추가하고 testRegex 함수 분리

--- src/hooks/useInputValidation.tsx
+++ src/hooks/useInputValidation.tsx
@@ -1,28 +1,33 @@
-import { useEffect, useState } from "react";
-import { ValidationResult } from "../pages/Signin";
+import { FormEvent, useEffect, useState } from "react";
+import { TestId, ValidationResult } from "../pages/Signin";
+import { userApis } from "../apis/user";
+import { useNavigate } from "react-router-dom";
 
 type AuthRegexKey = keyof typeof authRegex;
 type TestRegex = (type: AuthRegexKey, value: string) => boolean;
 export type UseFormValidation = typeof useFormValidation;
+export type HandleSubmit<T> = (e: FormEvent<HTMLFormElement>, path: T) => void;
 
 const authRegex = {
   email: /^(.+)@(.+)$/,
   password: /^.{9,}$/,
 };
 
+export const testRegex: TestRegex = (type, value) =>
+  authRegex[type].test(value);
+
 export const useFormValidation = () => {
+  const navigate = useNavigate();
+
   const [validationResult, setValidationResult] = useState<ValidationResult>({
     email: false,
     password: false,
   });
-
   const [isBtnDisabled, setIsBtnDisabled] = useState(false);
 
   useEffect(() => {
     checkValidationResult();
   }, [validationResult]);
-
-  const testRegex: TestRegex = (type, value) => authRegex[type].test(value);
 
   const checkValidationResult = () => {
     let key: keyof ValidationResult;
@@ -33,8 +38,31 @@ export const useFormValidation = () => {
     setIsBtnDisabled(true);
   };
 
+  const handleSubmit: HandleSubmit<TestId["button"]> = (e, path) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const payload = Object.fromEntries(formData) as Record<
+      TestId["input"],
+      string
+    >;
+
+    console.log(payload, path);
+
+    path === "signup" &&
+      userApis[path](payload)
+        .then((res) => {
+          const { status } = res;
+          if (status === 201) navigate("/signin");
+          console.log(status);
+        })
+        .catch((err) => console.log(err));
+
+    console.log("submit!");
+  };
+
   return {
-    testRegex,
+    handleSubmit,
     isBtnDisabled,
     validationResult,
     setValidationResult,
